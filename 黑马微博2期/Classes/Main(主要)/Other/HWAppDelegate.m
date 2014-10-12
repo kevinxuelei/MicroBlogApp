a2ca937e3c7b6223c58aba5ee34f1d497a07743e
apple <apple@Teacher-316.local>
2014-10-12 08:26:55 +0800

    OAuth授权

--- 黑马微博2期/Classes/Main(主要)/Other/HWAppDelegate.m
+++ 黑马微博2期/Classes/Main(主要)/Other/HWAppDelegate.m
@@ -9,6 +9,7 @@
 #import "HWAppDelegate.h"
 #import "HWTabBarViewController.h"
 #import "HWNewfeatureViewController.h"
+#import "HWOAuthViewController.h"
 
 @implementation HWAppDelegate
 
@@ -19,21 +20,22 @@
     self.window.frame = [UIScreen mainScreen].bounds;
     
     // 2.设置根控制器
-    NSString *key = @"CFBundleVersion";
-    // 上一次的使用版本（存储在沙盒中的版本号）
-    NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:key];
-    // 当前软件的版本号（从Info.plist中获得）
-    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
-    
-    if ([currentVersion isEqualToString:lastVersion]) { // 版本号相同：这次打开和上次打开的是同一个版本
-        self.window.rootViewController = [[HWTabBarViewController alloc] init];
-    } else { // 这次打开的版本和上一次不一样，显示新特性
-        self.window.rootViewController = [[HWNewfeatureViewController alloc] init];
-        
-        // 将当前的版本号存进沙盒
-        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
-        [[NSUserDefaults standardUserDefaults] synchronize];
-    }
+    self.window.rootViewController = [[HWOAuthViewController alloc] init];
+//    NSString *key = @"CFBundleVersion";
+//    // 上一次的使用版本（存储在沙盒中的版本号）
+//    NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+//    // 当前软件的版本号（从Info.plist中获得）
+//    NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
+//    
+//    if ([currentVersion isEqualToString:lastVersion]) { // 版本号相同：这次打开和上次打开的是同一个版本
+//        self.window.rootViewController = [[HWTabBarViewController alloc] init];
+//    } else { // 这次打开的版本和上一次不一样，显示新特性
+//        self.window.rootViewController = [[HWNewfeatureViewController alloc] init];
+//        
+//        // 将当前的版本号存进沙盒
+//        [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
+//        [[NSUserDefaults standardUserDefaults] synchronize];
+//    }
     
     // 3.显示窗口
     [self.window makeKeyAndVisible];
