f672ab416944670b85fa0b56d55001931a1ec534
apple <apple@Teacher-316.local>
2014-10-12 11:04:32 +0800

    封装控制器的切换

--- 黑马微博2期/Classes/Main(主要)/Other/HWAppDelegate.m
+++ 黑马微博2期/Classes/Main(主要)/Other/HWAppDelegate.m
@@ -7,8 +7,6 @@
 //
 
 #import "HWAppDelegate.h"
-#import "HWTabBarViewController.h"
-#import "HWNewfeatureViewController.h"
 #import "HWOAuthViewController.h"
 #import "HWAccount.h"
 #import "HWAccountTool.h"
@@ -23,23 +21,8 @@
     
     // 2.设置根控制器
     HWAccount *account = [HWAccountTool account];
-    
     if (account) { // 之前已经登录成功过
-        NSString *key = @"CFBundleVersion";
-        // 上一次的使用版本（存储在沙盒中的版本号）
-        NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:key];
-        // 当前软件的版本号（从Info.plist中获得）
-        NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
-
-        if ([currentVersion isEqualToString:lastVersion]) { // 版本号相同：这次打开和上次打开的是同一个版本
-            self.window.rootViewController = [[HWTabBarViewController alloc] init];
-        } else { // 这次打开的版本和上一次不一样，显示新特性
-            self.window.rootViewController = [[HWNewfeatureViewController alloc] init];
-
-            // 将当前的版本号存进沙盒
-            [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
-            [[NSUserDefaults standardUserDefaults] synchronize];
-        }
+        [self.window switchRootViewController];
     } else {
         self.window.rootViewController = [[HWOAuthViewController alloc] init];
     }
--- 黑马微博2期/Classes/OAuth(授权)/Controller/HWOAuthViewController.m
+++ 黑马微博2期/Classes/OAuth(授权)/Controller/HWOAuthViewController.m
@@ -8,8 +8,6 @@
 
 #import "HWOAuthViewController.h"
 #import "AFNetworking.h"
-#import "HWTabBarViewController.h"
-#import "HWNewfeatureViewController.h"
 #import "HWAccount.h"
 #import "HWAccountTool.h"
 #import "MBProgressHUD+MJ.h"
@@ -99,7 +97,6 @@
     // 1.请求管理者
     AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
 //    mgr.responseSerializer = [AFJSONResponseSerializer serializer];
-    
     // AFN的AFJSONResponseSerializer默认不接受text/plain这种类型
     
     // 2.拼接请求参数
@@ -120,22 +117,11 @@
         [HWAccountTool saveAccount:account];
         
         // 切换窗口的根控制器
-        NSString *key = @"CFBundleVersion";
-        // 上一次的使用版本（存储在沙盒中的版本号）
-        NSString *lastVersion = [[NSUserDefaults standardUserDefaults] objectForKey:key];
-        // 当前软件的版本号（从Info.plist中获得）
-        NSString *currentVersion = [NSBundle mainBundle].infoDictionary[key];
-        
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        if ([currentVersion isEqualToString:lastVersion]) { // 版本号相同：这次打开和上次打开的是同一个版本
-            window.rootViewController = [[HWTabBarViewController alloc] init];
-        } else { // 这次打开的版本和上一次不一样，显示新特性
-            window.rootViewController = [[HWNewfeatureViewController alloc] init];
-            
-            // 将当前的版本号存进沙盒
-            [[NSUserDefaults standardUserDefaults] setObject:currentVersion forKey:key];
-            [[NSUserDefaults standardUserDefaults] synchronize];
-        }
+        [window switchRootViewController];
+        
+        // UIWindow的分类、HWWindowTool
+        // UIViewController的分类、HWControllerTool
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         [MBProgressHUD hideHUD];
         HWLog(@"请求失败-%@", error);
