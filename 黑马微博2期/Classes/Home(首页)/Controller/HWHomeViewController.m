3067a206e2108c80cad9356e082c8080458f9ac7
apple <apple@Teacher-316.local>
2014-10-14 15:21:25 +0800

    显示微博未读数

--- 黑马微博2期/Classes/Home(首页)/Controller/HWHomeViewController.m
+++ 黑马微博2期/Classes/Home(首页)/Controller/HWHomeViewController.m
@@ -51,10 +51,49 @@
     // 集成上拉刷新控件
     [self setupUpRefresh];
     
-//    UIButton *btn = [UIButton buttonWithType:UIButtonTypeContactAdd];
-//    btn.y = 0;
-//    btn.x = 0;
-//    [self.view addSubview:btn];
+    // 获得未读数
+    NSTimer *timer = [NSTimer scheduledTimerWithTimeInterval:60 target:self selector:@selector(setupUnreadCount) userInfo:nil repeats:YES];
+    // 主线程也会抽时间处理一下timer（不管主线程是否正在其他事件）
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+}
+
+/**
+ *  获得未读数
+ */
+- (void)setupUnreadCount
+{
+//    HWLog(@"setupUnreadCount");
+//    return;
+    // 1.请求管理者
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    
+    // 2.拼接请求参数
+    HWAccount *account = [HWAccountTool account];
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"access_token"] = account.access_token;
+    params[@"uid"] = account.uid;
+    
+    // 3.发送请求
+    [mgr GET:@"https://rm.api.weibo.com/2/remind/unread_count.json" parameters:params success:^(AFHTTPRequestOperation *operation, NSDictionary *responseObject) {
+        // 微博的未读数
+//        int status = [responseObject[@"status"] intValue];
+        // 设置提醒数字
+//        self.tabBarItem.badgeValue = [NSString stringWithFormat:@"%d", status];
+        
+        // @20 --> @"20"
+        // NSNumber --> NSString
+        // 设置提醒数字(微博的未读数)
+        NSString *status = [responseObject[@"status"] description];
+        if ([status isEqualToString:@"0"]) { // 如果是0，得清空数字
+            self.tabBarItem.badgeValue = nil;
+            [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+        } else { // 非0情况
+            self.tabBarItem.badgeValue = status;
+            [UIApplication sharedApplication].applicationIconBadgeNumber = status.intValue;
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        HWLog(@"请求失败-%@", error);
+    }];
 }
 
 /**
@@ -123,8 +162,6 @@
         
         // 显示最新微博的数量
         [self showNewStatusCount:newStatuses.count];
-        
-//        HWLog(@"%@", NSStringFromUIEdgeInsets(self.tableView.contentInset));
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         HWLog(@"请求失败-%@", error);
         
@@ -183,6 +220,10 @@
  */
 - (void)showNewStatusCount:(int)count
 {
+    // 刷新成功(清空图标数字)
+    self.tabBarItem.badgeValue = nil;
+    [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+    
     // 1.创建label
     UILabel *label = [[UILabel alloc] init];
     label.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"timeline_new_status_background"]];
@@ -208,14 +249,12 @@
     // 先利用1s的时间，让label往下移动一段距离
     CGFloat duration = 1.0; // 动画的时间
     [UIView animateWithDuration:duration animations:^{
-//        label.y += label.height;
         label.transform = CGAffineTransformMakeTranslation(0, label.height);
     } completion:^(BOOL finished) {
         // 延迟1s后，再利用1s的时间，让label往上移动一段距离（回到一开始的状态）
         CGFloat delay = 1.0; // 延迟1s
         // UIViewAnimationOptionCurveLinear:匀速
         [UIView animateWithDuration:duration delay:delay options:UIViewAnimationOptionCurveLinear animations:^{
-//            label.y -= label.height;
             label.transform = CGAffineTransformIdentity;
         } completion:^(BOOL finished) {
             [label removeFromSuperview];
@@ -362,7 +401,6 @@
     if (self.statuses.count == 0 || self.tableView.tableFooterView.isHidden == NO) return;
     
     CGFloat offsetY = scrollView.contentOffset.y;
-    
     // 当最后一个cell完全显示在眼前时，contentOffset的y值
     CGFloat judgeOffsetY = scrollView.contentSize.height + scrollView.contentInset.bottom - scrollView.height - self.tableView.tableFooterView.height;
     if (offsetY >= judgeOffsetY) { // 最后一个cell完全进入视野范围内
@@ -372,13 +410,5 @@
         // 加载更多的微博数据
         [self loadMoreStatus];
     }
-    
-    /*
-     contentInset：除具体内容以外的边框尺寸
-     contentSize: 里面的具体内容（header、cell、footer），除掉contentInset以外的尺寸
-     contentOffset: 
-     1.它可以用来判断scrollView滚动到什么位置
-     2.指scrollView的内容超出了scrollView顶部的距离（除掉contentInset以外的尺寸）
-     */
 }
 @end
